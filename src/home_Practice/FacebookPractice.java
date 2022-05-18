10ba2aa3b777fb26beeae8744b05531f43a8f488
meldasav <gmeldag83@hotmail.com>
2022-05-18 11:39:19 -0500

    Facebook validation

--- src/home_Practice/FacebookPractice.java
+++ src/home_Practice/FacebookPractice.java
@@ -1,34 +1,78 @@
 package home_Practice;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import test.ValidationUtils;
 import utilities.Driver;
 
+import static test.ValidationUtils.*;
 public class FacebookPractice {
     public static void main(String[] args) {
-            /**
-             * 1. Go to https://www.facebook.com/
-             * 2. Validate the title of the page is “Facebook - Log In or Sign Up”
-             * 3. Validate the URL of the page is “https://www.facebook.com/”
-             * 4. Validate “facebook” logo is displayed
-             * 5. Validate heading2 “Connect with friends and the world  around you on Facebook.” is displayed
-             * 6. Validate “Email or Phone Number” input box is displayed
-             * 7. Validate “Password” input box is displayed
-             * 8. Validate “Log In” button is displayed and enabled
-             * 9. Validate “Forgot Password?” link is displayed
-             * 10. Validate “Create New Account” button is displayed
+        /**
+         * 1. Go to https://www.facebook.com/
+         * 2. Validate the title of the page is “Facebook - Log In or Sign Up”
+         * 3. Validate the URL of the page is “https://www.facebook.com/”
+         * 4. Validate “facebook” logo is displayed
+         * 5. Validate heading2 “Connect with friends and the world  around you on Facebook.” is displayed
+         * 6. Validate “Email or Phone Number” input box is displayed
+         * 7. Validate “Password” input box is displayed
+         * 8. Validate “Log In” button is displayed and enabled
+         * 9. Validate “Forgot Password?” link is displayed
+         * 10. Validate “Create New Account” button is displayed
+         */
+        WebDriver driver = Driver.getDriver();
+
+
+        try {
+            // get the website
+            driver.get("https://www.facebook.com/");
+
+            String expectedTitle = "Facebook - log in or sign up";
+            validateTitle(driver, expectedTitle);
+
+            String expectedUrl = "https://www.facebook.com/";
+            validateURL(driver, expectedUrl);
+
+            /*
+            xpath --> //tagName[@attributeName = 'attributeValue']
+            css   --> tagName[attributeName = 'attributeValue']
              */
-                WebDriver driver = Driver.getDriver();
-                try {
-                    // get the website
-                    driver.get("https://www.facebook.com/");
+            //                         xpath -->   //img[@class = 'fb_logo _8ilh img']
+            //                         css   -->     img[class = 'fb_logo _8ilh img']
 
-                    String expectedTitle = "Facebook - Log In or Sign Up";
-                    ValidationUtils.validateTitle(driver, expectedTitle);
-                } finally {
-                    Driver.quitDriver();
-                }
+//            WebElement fbLogo = driver.findElement(By.className("fb_logo"));
+//            WebElement fbLogo = driver.findElement(By.cssSelector(".fb_logo"));
+//            WebElement fbLogo = driver.findElement(By.xpath("//img[@class = 'fb_logo _8ilh img']"));
+            WebElement fbLogo = driver.findElement(By.cssSelector("img[class = 'fb_logo _8ilh img']"));
+            validateElementIsDisplayed(fbLogo, "Facebook logo");
 
-            }
+//            WebElement fbHeading2 = driver.findElement(By.className("_8eso"));
+            WebElement fbHeading2 = driver.findElement(By.cssSelector("._8eso"));
+//            WebElement fbHeading2 = driver.findElement(By.xpath("//h2[@class='_8eso']"));
+//            WebElement fbHeading2 = driver.findElement(By.cssSelector("h2[class='_8eso']"));
+            validateElementIsDisplayed(fbHeading2, "Facebook header");
+
+            WebElement inputEmailPhone = driver.findElement(By.id("email"));
+            validateElementIsDisplayed(inputEmailPhone, "Email or phone number input box");
+
+            WebElement inputPassword = driver.findElement(By.id("pass"));
+            validateElementIsDisplayed(inputPassword, "Password input box");
+
+            WebElement loginButton = driver.findElement(By.name("login"));
+            validateElementIsDisplayedAndEnabled(loginButton, "Login button");
+
+            WebElement forgotPasswordLink = driver.findElement(By.linkText("Forgot password?"));
+            validateElementIsDisplayed(forgotPasswordLink, "Forgot password? link");
+
+//            WebElement createNewAccountButton = driver.findElement(By.xpath("//a[@data-testid='open-registration-form-button']"));
+            WebElement createNewAccountButton = driver.findElement(By.cssSelector("a[data-testid='open-registration-form-button']"));
+            validateElementIsDisplayed(createNewAccountButton, "Create New Account button");
+
+
+        } finally {
+            Driver.quitDriver();
         }
+    }
 
+}
--- src/test/ValidationUtils.java
+++ src/test/ValidationUtils.java
@@ -1,7 +1,9 @@
 package test;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+import test.ValidationUtils.*;
 public class ValidationUtils {
 
     public static void validateTitle(WebDriver driver, String expectedTitle) {
@@ -10,8 +12,45 @@ public class ValidationUtils {
                 "Expected title: \"" + expectedTitle + "\" | Actual Title: \"" + driver.getTitle() + "\"");
     }
 
-    public static void validateTitle1(WebDriver driver,String expectedTitle){
+    public static void validateTitle1(WebDriver driver, String expectedTitle) {
         if (driver.getTitle().equals(expectedTitle)) System.out.println("Title is VALIDATED");
         else System.out.println("Title is not VALIDATED");
     }
+
+    public static void validateURL(WebDriver driver, String expectedURL) {
+        if (driver.getCurrentUrl().equals(expectedURL)) System.out.println("URL is validated");
+        else throw new RuntimeException("URL is validated\n" +
+                "Expected URL: \"" + expectedURL + "\" | Actual URL: \"" + driver.getCurrentUrl() + "\"");
+    }
+
+    public static void validateElementIsDisplayed(WebElement element) {
+        if (element.isDisplayed()) System.out.println("Element is Displayed");
+        else throw new RuntimeException("Element is not Displayed");
+    }
+
+    public static void validateElementIsDisplayed(WebElement element, String elementName) {
+        if (element.isDisplayed()) System.out.println(elementName + "Displayed");
+        else throw new RuntimeException(elementName + "is not Displayed");
+    }
+    public static void validateElementIsEnabled(WebElement element) {
+        if (element.isEnabled()) System.out.println("Element is ENABLED");
+        else throw new RuntimeException("Element is NOT ENABLED");
+    }
+
+    public static void validateElementIsEnabled(WebElement element, String elementName) {
+        if (element.isEnabled()) System.out.println(elementName + " is ENABLED");
+        else throw new RuntimeException(elementName + " is NOT ENABLED");
+    }
+
+    public static void validateElementIsDisplayedAndEnabled(WebElement element) {
+        validateElementIsDisplayed(element);
+        validateElementIsEnabled(element);
+    }
+
+    public static void validateElementIsDisplayedAndEnabled(WebElement element, String elementName) {
+        validateElementIsDisplayed(element, elementName);
+        validateElementIsEnabled(element, elementName);
+    }
+
+
 }
